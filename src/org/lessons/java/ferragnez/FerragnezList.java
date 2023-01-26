4a5a43890f5e93cb23652e6dc7604a1fe47f6e96
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-01-26 16:35:25 +0100

    ferragnez

---
+++ src/org/lessons/java/ferragnez/FerragnezList.java
@@ -0,0 +1,51 @@
+package org.lessons.java.ferragnez;
+
+import java.util.Scanner;
+
+public class FerragnezList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		State lavorando col servizio di sicurezza dei Ferragnez e il vostro compito è di 
+//		assicurarvi che accedano alla festa solo gli invitati presenti sulla lista (in fondo 
+//		al post) Consegna: creare un progetto java-festa-ferragnez con il package 
+//		com.ferragnez.party, a cui aggiungere la classe CheckGuest Nel programma bisogna: 
+//		creare e inizializzare l’array contenente i nomi degli invitati chiedere all’utente 
+//		come si chiama verificare che il nome sia presente nella lista lasciarlo entrare o 
+//		rispedirlo cortesemente da dove è venuto :occhiali_da_sole: Bonus se avete usato il 
+//		ciclo for per cercare il nome nella lista nella consegna base, implementare lo stesso 
+//		programma usando il ciclo while, e viceversa Buon lavoro e buon weekend! 
+//		:faccia_leggermente_sorridente: :tada: Lista invitati: Dua Lipa, Paris Hilton, 
+//		Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, 
+//		Martina Maccherone, Rachel Zeili
+//			
+			Scanner guestName = new Scanner(System.in);
+		
+			String[] invited = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei" };
+			String invitedName;
+			
+			
+			
+			System.out.println("Buonasera Signor*, il suo nome prego..");
+			invitedName = guestName.nextLine();
+			
+			for(int i = 0; i < invited.length; i++) {
+				String bodyGuardMessage;
+				
+				String nameList = invited[i].toLowerCase();
+				
+				if(invitedName.equals(nameList) ) {
+					bodyGuardMessage = "Prego, si accomodi..";
+					System.out.println(bodyGuardMessage);
+					
+				}else{
+					bodyGuardMessage = "Ma ndo vai?";
+					System.out.println(bodyGuardMessage);
+				}
+
+			}
+	}
+
+}
